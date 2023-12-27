a8d6a4cda7d7a73a6010431f54fa689331c02f6c
it20117764 <it20117764@my.sliit.lk>
2023-12-27 22:43:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import NavBar from './components/NavBar';
+import './styles/common.css'
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import TravelerManagementPage from './pages/TravelerManagementPage';
+import TrainManagementPage from './pages/TrainManagementPage';
+import TicketBookingManagementPage from './pages/TicketBookingManagementPage';
+import SignUpPage from './pages/SignUpPage';
+
+
+function App() {
+
+  const [isLogged, setIsLogged] = useState(sessionStorage.getItem("isLogged"));
+  const [token, setToken] = useState(sessionStorage.getItem("token"));
+  const [username, setUserName] = useState(sessionStorage.getItem("username"));
+  const [userId, setUserId] = useState(sessionStorage.getItem("userId"));
+  const [role, setrole] = useState(sessionStorage.getItem("role"));
+
+  return (
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
+      {isLogged == "true" ?
+        <>
+          <div className='row' style={{ height: '100%', margin: '0%' }}>
+            <Router >
+              <NavBar />
+              <div className="col-lg-10" style={{ margin: '0%', padding: '0%' }}>
+                <Routes>
+                  <Route exact path='*' element={<TravelerManagementPage />} />
+                  <Route exact path='/travelerManagement' element={<TravelerManagementPage />} />
+                  {role != null && role == "1" &&
+                    <Route exact path='/trainManagement' element={<TrainManagementPage />} />
+                  }
+                  <Route exact path='/ticketManagement' element={<TicketBookingManagementPage />} />
+                </Routes>
+              </div>
+            </Router>
+          </div>
+        </> :
+        <>
+          <div className='row' style={{ height: '100%', margin: '0%' }}>
+            <Router >
+              <Routes>
+                <Route exact path='*' element={<LoginPage />} />
+                <Route exact path='/login' element={<LoginPage />} />
+                <Route exact path='/register' element={<SignUpPage />} />
+              </Routes>
+            </Router>
+          </div>
+        </>
+
+      }
+
+      <ToastContainer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/services/RestService.js
@@ -0,0 +1,168 @@
+import axios from 'axios';
+
+const BACKEND_URL = "http://localhost:7211/api/";
+
+
+/**
+ * Rest Service APIs
+ */ 
+class RestService {
+
+
+    register(userDto) {
+        return axios.post(BACKEND_URL + 'Authentication/RegisterUser',userDto)
+    }
+
+    authenticateUser(username, password) {
+        return axios.post(BACKEND_URL + 'Authentication/UserLogin',{userName:username, password:password})
+    }
+
+
+    addTrain(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Train/addTrain',req, config)
+    }
+
+
+    getSchedulesByTrainId(token, code){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Train/getSheduleByTrainId?TrainCode',{trainCode:code}, config)
+    }
+
+
+    getAllTrains(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.get(BACKEND_URL + 'Train/getAllTrain', config)
+    }
+
+    addSchedule(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Train/addSchedule',req, config)
+    }
+
+    getAllUsers(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.get(BACKEND_URL + 'User/GetAllUsers', config)
+    }
+
+    deleteUserByNIC(token, id){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.delete(BACKEND_URL + 'User/deletAccountById?nic='+ id, config)
+    }
+
+    updateUserById(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'User/updateAccountById',req, config)
+    }
+
+
+    getAllReservations(token){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.get(BACKEND_URL + 'Ticket/getAllReservation', config)
+    }
+
+    getAllSchedules(token){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.get(BACKEND_URL + 'Train/getAllSchedule', config)
+    }
+
+    addReservation(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Ticket/addReservation',req, config)
+    }
+
+    updateReservationById(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Ticket/updateReservationById',req, config)
+    }
+
+    updateScheduleById(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Train/updateScheduleById',req, config)
+    }
+
+    updateTrainById(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Train/updateTrainById',req, config)
+    }
+
+
+    cancelTrainById(token, req){
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': "*",
+                'Authorization': 'Bearer ' + token
+            }
+        }
+        return axios.post(BACKEND_URL + 'Train/cancelTrain',req, config)
+    }
+
+
+}
+
+export default new RestService();
